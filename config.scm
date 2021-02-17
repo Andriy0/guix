56f21448c24e578832d9e7f63bc168093dc397e2
Andriy0 <56863249+Andriy0@users.noreply.github.com>
2021-02-17 16:32:37 +0200

    update

--- config.scm
+++ config.scm
@@ -3,30 +3,35 @@
 ;; environments.
 
 (use-modules 
-  (gnu) 
-  (srfi srfi-1)
-  (gnu system nss) 
-  (gnu services xorg)
-  (gnu services pm)
-  (gnu services networking)
-  (gnu services desktop)
-  (gnu packages gnome) 
-  (gnu packages linux)
-  (gnu packages vim)
-  (gnu packages gtk)
-  (gnu packages xorg)
-  (gnu packages emacs)
-  (gnu packages audio)
-  (gnu packages pulseaudio)
-  (gnu packages version-control)
-  (nongnu packages linux) 
-  (nongnu system linux-initrd))
+ (srfi srfi-1)
+ (guix channels)
+ (guix inferior)
+ (gnu) 
+ (gnu system nss) 
+ (gnu services pm)
+ (gnu services sound)
+ (gnu services dbus)
+ (gnu services nix)
+ (gnu services sysctl)
+ (gnu services sddm)
+ (gnu services audio)
+ (gnu services networking)
+ (gnu packages vim)
+ (gnu packages pulseaudio)
+ (gnu packages package-management)
+ (gnu packages version-control)
+ (gnu packages xorg)
+ (gnu packages xdisorg)
+ (gnu packages linux)
+ (gnu packages audio)
+ (gnu packages gnome)
+ (nongnu packages linux) 
+ (nongnu system linux-initrd))
 
-(use-service-modules desktop)
+(use-service-modules desktop xorg)
 
-(use-package-modules bootloaders certs 
-emacs wm
-xorg terminals)
+(use-package-modules bootloaders certs emacs emacs-xyz
+		     ratpoison suckless wm xorg terminals)
 
 ;; Allow members of the "video" group to change the screen brightness.
 (define %backlight-udev-rule
@@ -40,16 +45,26 @@ xorg terminals)
 
 (define %my-desktop-services
   (modify-services %desktop-services
-                   (elogind-service-type config =>
-                                         (elogind-configuration (inherit config)
-                                                                (handle-lid-switch-external-power 'suspend)))
-                   (udev-service-type config =>
-                                      (udev-configuration (inherit config)
-                                                          (rules (cons %backlight-udev-rule
-                                                                       (udev-configuration-rules config)))))
-                   (network-manager-service-type config =>
-                                                 (network-manager-configuration (inherit config)
-                                                                                (vpn-plugins (list network-manager-openvpn))))))
+		   (elogind-service-type config =>
+					 (elogind-configuration (inherit config)
+								(handle-lid-switch-external-power 'suspend)))
+		   (udev-service-type config =>
+				      (udev-configuration (inherit config)
+							  (rules (cons %backlight-udev-rule
+								       (udev-configuration-rules config)))))
+		   (guix-service-type config =>
+				      (guix-configuration
+				       (inherit config)
+				       (substitute-urls (append
+							 %default-substitute-urls
+							 (list "https://mirror.brielmaier.net")))
+				       (authorized-keys (append
+							 %default-authorized-guix-keys
+							 (list (local-file "mirror.brielmaier.net.pub"))))))
+		   
+		   (network-manager-service-type config =>
+						 (network-manager-configuration (inherit config)
+										(vpn-plugins (list network-manager-openvpn))))))
 
 (define %xorg-libinput-config
   "Section \"InputClass\"
@@ -57,126 +72,184 @@ xorg terminals)
   Driver \"libinput\"
   MatchDevicePath \"/dev/input/event*\"
   MatchIsTouchpad \"on\"
-
   Option \"Tapping\" \"on\"
+  Option \"TappingButtonMap\" \"lmr\"
   Option \"TappingDrag\" \"on\"
-  Option \"DisableWhileTyping\" \"on\"
+  Option \"DisableWhileTyping\" \"off\"
   Option \"MiddleEmulation\" \"on\"
   Option \"ScrollMethod\" \"twofinger\"
+  Option \"NaturalScrolling\" \"true\"
 EndSection
+
 Section \"InputClass\"
   Identifier \"Keyboards\"
   Driver \"libinput\"
   MatchDevicePath \"/dev/input/event*\"
   MatchIsKeyboard \"on\"
+  Option \"XkbLayout\" \"us,ua\"
+  Option \"XkbModel\" \"pc105\"
+  Option \"XkbOptions\" \"caps:ctrl_modifier,altwin:menu_win,grp:ctrls_toggle\"
 EndSection
 ")
 
 (operating-system
-  (kernel linux)
-  (initrd microcode-initrd)
-  (firmware (list linux-firmware))
+ ;; (kernel 
+ ;;  (let*
+ ;;      ((channels
+ ;;        (list (channel
+ ;;               (name 'nonguix)
+ ;;               (url "https://gitlab.com/nonguix/nonguix")
+ ;;               (commit "54af5777060401d596e20f26a3cad455a07d0018"))
+ ;;              (channel
+ ;;               (name 'guix)
+ ;;               (url "https://git.savannah.gnu.org/git/guix.git")
+ ;;               (commit "6340fe6f0c06386f0c9db7dee36d1d737db0e568"))))
+ ;;       (inferior
+ ;;        (inferior-for-channels channels)))
+ ;;    (first (lookup-inferior-packages inferior "linux" "5.10.8"))))
+ (kernel linux)
+ (initrd microcode-initrd)
+ (firmware (list linux-firmware))
 
-  (host-name "guixsd")
-  (timezone "Europe/Kiev")
-  (locale "en_US.utf8")
+ (host-name "guixsd")
+ (timezone "Europe/Kiev")
+ (locale "en_US.utf8")
 
-  ;; Use the UEFI variant of GRUB with the EFI System
-  ;; Partition mounted on /boot/efi.
-  (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (target "/boot/efi")))
+ ;; Use the UEFI variant of GRUB with the EFI System
+ ;; Partition mounted on /boot/efi.
+ (bootloader (bootloader-configuration
+              (bootloader grub-efi-bootloader)
+              (target "/boot/efi")))
 
-  ;; Assume the target root file system is labelled "my-root",
-  ;; and the EFI System Partition has UUID 1234-ABCD.
-  (file-systems (append
-                 (list (file-system
-                         (device (file-system-label "guixsd"))
-                         (mount-point "/")
-                         (type "btrfs")
-                         (options "subvol=@"))
-                       (file-system
-                         (device (file-system-label "guixsd"))
-                         (mount-point "/home")
-                         (type "btrfs")
-                         (options "subvol=@home"))
-                       (file-system
-                         (device (file-system-label "guixsd"))
-                         (mount-point "/gnu")
-                         (type "btrfs")
-                         (options "subvol=@gnu"))
-                       (file-system
-                         (device (file-system-label "guixsd"))
-                         (mount-point "/var")
-                         (type "btrfs")
-                         (options "subvol=@var"))
-                       (file-system
-                         (device (file-system-label "ESP"))
-                         (mount-point "/boot/efi")
-                         (type "vfat")))
-                 %base-file-systems))
+ (kernel-arguments
+  (append
+   (list "modprobe.blacklist=pcspkr"
+	 "intel_pstate=disable"
+	 "snd_hda_intel.index=1")
+   %default-kernel-arguments))
 
-  (users (cons (user-account
-                (name "andriy")
-                (comment "Andrii")
-                (group "users")
-                (supplementary-groups '("wheel" "netdev"
-                                        "audio" "video")))
-               %base-user-accounts))
+ ;; Assume the target root file system is labelled "my-root",
+ ;; and the EFI System Partition has UUID 1234-ABCD.
+ (file-systems (append
+                (list (file-system
+                       (device (file-system-label "guixsd"))
+                       (mount-point "/")
+                       (type "btrfs")
+		       (flags '(no-atime))
+                       (options "ssd,space_cache,compress=zstd,commit=120,subvol=@"))
+                      (file-system
+                       (device (file-system-label "guixsd"))
+                       (mount-point "/home")
+                       (type "btrfs")
+                       (options "subvol=@home"))
+                      (file-system
+                       (device (file-system-label "guixsd"))
+                       (mount-point "/gnu")
+                       (type "btrfs")
+		       (flags '(no-atime))
+                       (options "subvol=@gnu"))
+		      ;; It didn't work with /var on separate subvol
+                      ;; (file-system
+                      ;;   (device (file-system-label "guixsd"))
+                      ;;   (mount-point "/var")
+                      ;;   (type "btrfs")
+                      ;;   (options "subvol=@var"))
+                      (file-system
+                       (device (file-system-label "ESP"))
+                       (mount-point "/boot/efi")
+                       (type "vfat")
+		       (flags '(no-atime))
+		       (check? #f)))
+                %base-file-systems))
 
-  ;; Add a bunch of window managers; we can choose one at
-  ;; the log-in screen with F1.
-  (packages (append (list
-                     ;; window managers
-                     awesome 
-                     ;; editors
-                     emacs vim
-                     ;; terminal emulators
-                     xterm alacritty
-                     ;; tools
-                     git gvfs
-                     ;; misc
-                     bluez bluez-alsa
-                     pulseaudio tlp
-                     xf86-input-libinput
-                     ;; for HTTPS access
-                     nss-certs)
-                    %base-packages))
+ (users (cons (user-account
+               (name "andriy")
+               (comment "Andrii")
+               (group "users")
+               (supplementary-groups '("wheel" "netdev"
+                                       "audio" "video"
+				       "lp")))
+              %base-user-accounts))
 
-  ;; Use the "desktop" services, which include the X11
-  ;; log-in service, networking with NetworkManager, and more.
-  ;; (services %desktop-services)
+ ;; Add the 'realtime' group
+ (groups (cons (user-group (system? #t) (name "realtime"))
+               %base-groups))
 
-  (services (cons* (service slim-service-type
-                            (slim-configuration
-                              (xorg-configuration
-                                (xorg-configuration
-                                  ;; (keyboard-layout keyboard-layout)
-                                  (extra-config (list %xorg-libinput-config))))))
-                   (service tlp-service-type
-                            (tlp-configuration
-                              (cpu-boost-on-ac? #t)
-                              (wifi-pwr-on-bat? #t)))
-                   (pam-limits-service ;; This enables JACK to enter realtime mode
-                     (list
-                       (pam-limits-entry "@realtime" 'both 'rtprio 99)
-                       (pam-limits-entry "@realtime" 'both 'memlock 'unlimited)))
-                   (service thermald-service-type)
-                   ;; (service docker-service-type)
-                   ;; (service libvirt-service-type
-                   ;;          (libvirt-configuration
-                   ;;            (unix-sock-group "libvirt")
-                   ;;            (tls-port "16555")))
-                   ;; (service cups-service-type
-                   ;;          (cups-configuration
-                   ;;            (web-interface? #t)
-                   ;;            (extensions
-                   ;;              (list cups-filters))))
-                   ;; (service nix-service-type)
-                   (bluetooth-service #:auto-enable? #t)
-                   (remove (lambda (service)
-                             (eq? (service-kind service) gdm-service-type))
-                           %my-desktop-services)))
+ ;; Add a bunch of window managers; we can choose one at
+ ;; the log-in screen with F1.
+ (packages (append (list
+                    ;; window managers
+                    awesome
+		    emacs-exwm emacs-desktop-environment
+		    ;; editors
+                    emacs vim neovim
+                    ;; terminal emulator
+                    xterm alacritty
+		    ;; mics
+		    git xf86-input-libinput
+		    pulseaudio
+		    alsa-utils
+		    bluez bluez-alsa
+		    tlp gvfs cpupower
+		    acpi
+                    ;; for HTTPS access
+                    nss-certs)
+                   %base-packages))
 
-  ;; Allow resolution of '.local' host names with mDNS.
-  (name-service-switch %mdns-host-lookup-nss))
+ ;; Use the "desktop" services, which include the X11
+ ;; log-in service, networking with NetworkManager, and more.
+ ;; (services %desktop-services)
+
+ (services
+  (cons*
+   ;; (service slim-service-type
+   ;; 	     (slim-configuration
+   ;; 	      (xorg-configuration
+   ;; 	       (xorg-configuration
+   ;; 		(extra-config (list %xorg-libinput-config))))))
+   (service sddm-service-type
+	    (sddm-configuration
+	     (numlock "off")
+	     (theme "maldives")
+	     (xsessions-directory "/etc/X11/xsessions")
+	     (xorg-configuration
+	      (xorg-configuration
+	       (extra-config (list %xorg-libinput-config))))))
+   (service tlp-service-type
+	    (tlp-configuration
+	     (cpu-boost-on-ac? #f)
+	     (cpu-boost-on-bat? #f)
+	     (cpu-scaling-governor-on-ac (list "ondemand"))
+	     (cpu-scaling-governor-on-bat (list "ondemand"))
+	     (cpu-scaling-max-freq-on-ac 1500000)
+	     (cpu-scaling-max-freq-on-bat 1500000)))
+   (pam-limits-service ;; This enables JACK to enter realtime mode
+    (list
+     (pam-limits-entry "@realtime" 'both 'rtprio 99)
+     (pam-limits-entry "@realtime" 'both 'memlock 'unlimited)))
+   ;; (service mpd-service-type
+   ;;          (mpd-configuration
+   ;;           (user "andriy")
+   ;; 	     (music-dir "~/Music")
+   ;;           (playlist-dir "~/.local/share/mpd/playlists")
+   ;;           (db-file "~/.local/share/mpd/tag_cache")
+   ;;           (state-file "~/.local/share/mpd/state")
+   ;;           (sticker-file "~/.local/share/mpd/sticker.sql")
+   ;; 	     (port "6600")
+   ;;           (address "localhost")))
+   ;; (service alsa-service-type
+   ;; 	     (alsa-configuration
+   ;; 	      (pulseaudio? #f)))
+   (screen-locker-service xscreensaver "xscreensaver")
+   (service sysctl-service-type
+            (sysctl-configuration
+             (settings '(("dev.i915.perf_stream_paranoid" ."0")))))
+   (service nix-service-type)
+   (bluetooth-service #:auto-enable? #f)
+   (remove (lambda (service)
+	     (eq? (service-kind service) gdm-service-type))
+	   %my-desktop-services)))
+
+ ;; Allow resolution of '.local' host names with mDNS.
+ (name-service-switch %mdns-host-lookup-nss))
+
